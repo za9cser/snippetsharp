25462deaef2fe94751c10f2a939f27e0c693e692
Sergei Zaitsev <zaitsev.sergey.93@gmail.com>
2023-01-28 19:35:01 +0300

    fix: fix snippets

--- USnippetPack.VS2022/Properties/AssemblyInfo.cs
+++ USnippetPack.VS2022/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SnippetSharp")]
+[assembly: AssemblyTitle("USnippetPack.VS2022")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SnippetSharp")]
+[assembly: AssemblyProduct("USnippetPack.VS2022")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
